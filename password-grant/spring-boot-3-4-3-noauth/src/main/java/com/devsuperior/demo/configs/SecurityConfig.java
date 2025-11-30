9b7b27d6f326b4055e7a2395ed3ff2d48ec5d8f2
gustcustodio <gustcustodio@gmail.com>
2025-11-30 09:30:59 -0300

    adicionando Spring Security ao projeto

---
+++ password-grant/spring-boot-3-4-3-noauth/src/main/java/com/devsuperior/demo/configs/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.devsuperior.demo.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable());
+        http.authorizeHttpRequests(auth -> {
+            auth.requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll();
+            auth.anyRequest().permitAll();
+        });
+        http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
+        return http.build();
+    }
+
+}
