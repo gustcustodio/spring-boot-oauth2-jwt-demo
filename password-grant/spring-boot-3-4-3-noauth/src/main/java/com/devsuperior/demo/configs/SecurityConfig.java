a490598f2be4d3db624d350ccf90564f5dd7f785
gustcustodio <gustcustodio@gmail.com>
2025-12-01 06:15:50 -0300

    BCrypt password encoder

--- password-grant/spring-boot-3-4-3-noauth/src/main/java/com/devsuperior/demo/configs/SecurityConfig.java
+++ password-grant/spring-boot-3-4-3-noauth/src/main/java/com/devsuperior/demo/configs/SecurityConfig.java
@@ -3,11 +3,18 @@ package com.devsuperior.demo.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
--- password-grant/spring-boot-3-4-3-noauth/src/main/resources/import.sql
+++ password-grant/spring-boot-3-4-3-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$GOaIaZ299tzMoE3pL6RNT.ic5vPqpXIu.KM7B3zCk776vjBN1m/NW');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$GOaIaZ299tzMoE3pL6RNT.ic5vPqpXIu.KM7B3zCk776vjBN1m/NW');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
